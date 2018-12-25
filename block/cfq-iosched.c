facefa00bc4b0f2dc40ceb549042bff208a4c7bf
dkati <dkatikaridis@gmail.com>
2018-12-25 20:12:46 +0200

    cfq: Update it for SSDs
    our storage is considered as ssd
    
    Change-Id: I910473318b369cefffb0f5167d9cb95ddeb3bab0

--- block/cfq-iosched.c
+++ block/cfq-iosched.c
@@ -3754,6 +3754,18 @@ static void *cfq_init_queue(struct request_queue *q)
 	return cfqd;
 }
 
+static void cfq_registered_queue(struct request_queue *q)
+{
+	struct elevator_queue *e = q->elevator;
+	struct cfq_data *cfqd = e->elevator_data;
+
+	/*
+	 * Default to IOPS mode with no idling for SSDs
+	 */
+	if (blk_queue_nonrot(q))
+		cfqd->cfq_slice_idle = 0;
+}
+
 /*
  * sysfs parts below -->
  */
@@ -3867,8 +3879,9 @@ static struct elevator_type iosched_cfq = {
 		.elevator_set_req_fn =		cfq_set_request,
 		.elevator_put_req_fn =		cfq_put_request,
 		.elevator_may_queue_fn =	cfq_may_queue,
-		.elevator_init_fn =		cfq_init_queue,
-		.elevator_exit_fn =		cfq_exit_queue,
+		.elevator_init_fn =			cfq_init_queue,
+		.elevator_exit_fn =			cfq_exit_queue,
+		.elevator_registered_fn =	cfq_registered_queue,
 	},
 	.icq_size	=	sizeof(struct cfq_io_cq),
 	.icq_align	=	__alignof__(struct cfq_io_cq),
