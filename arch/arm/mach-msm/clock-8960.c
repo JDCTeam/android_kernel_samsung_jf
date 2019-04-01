6a9e9db9ab1d72713eb45c0f057f84c6a5462e16
dkati <dkatikaridis@gmail.com>
2019-04-01 16:11:00 +0300

    gpu: Reduce gpu voltage by 25ma

--- arch/arm/mach-msm/clock-8960.c
+++ arch/arm/mach-msm/clock-8960.c
@@ -426,12 +426,12 @@ static int set_vdd_dig_8960(struct clk_vdd_class *vdd_class, int level)
 {
 	static const int vdd_uv[] = {
 		[VDD_DIG_NONE]    =       0,
-		[VDD_DIG_LOW]     =  945000,
-		[VDD_DIG_NOMINAL] = 1050000,
-		[VDD_DIG_HIGH]    = 1150000
+		[VDD_DIG_LOW]     =  920000,
+		[VDD_DIG_NOMINAL] = 1025000,
+		[VDD_DIG_HIGH]    = 1125000
 	};
 	return rpm_vreg_set_voltage(RPM_VREG_ID_PM8921_S3, RPM_VREG_VOTER3,
-				    vdd_uv[level], 1150000, 1);
+				    vdd_uv[level], 1125000, 1);
 }
 #endif
 
