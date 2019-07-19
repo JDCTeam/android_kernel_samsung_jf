35ca661e4e953b2babb6497a81374b31230f3fb5
1bdellatiF <ptaoubi@gmail.com>
2019-07-19 20:47:31 +0100

    Entropy Tweaking

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -298,7 +298,7 @@ static int random_read_wakeup_thresh = 64;
 #ifdef CONFIG_CRYPTO_FIPS
 static int random_write_wakeup_thresh = 320;
 #else
-static int random_write_wakeup_thresh = 128;
+static int random_write_wakeup_thresh = 256;
 #endif
 
 /*
